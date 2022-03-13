31cdf0e365ba202d1c52971308a84fe808d10f76
r-i-c-h <rich@richwerden.com>
2022-03-13 04:21:04 -0400

    add:core🎨+basic Signup/Login layout w/dummy functionality

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,14 +1,37 @@
+import { FormEvent, useState } from 'react'
 
-import './Login.module.css';
+// styles
+import styles from './Login.module.css'
 
-function Login() {
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div className="Login">
-      <header className="Login-header">
-        <h1>Login!</h1>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
   )
 }
-
-export default Login
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,14 +1,46 @@
+import { FormEvent, useState } from 'react'
 
-import './Signup.module.css';
+// styles
+import styles from './Signup.module.css'
 
-function Signup() {
+export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    console.log(email, password, displayName)
+  }
+
   return (
-    <div className="Signup">
-      <header className="Signup-header">
-        <h1>Signup!</h1>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
   )
 }
-
-export default Signup
