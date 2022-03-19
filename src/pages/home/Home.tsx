e52220aafc221a0b57977f51239cef2e31c40a96
r-i-c-h <rich@richwerden.com>
2022-03-19 17:05:17 -0400

    🆙🔧 Home to request only current user +triv🎨

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useCollection } from '../../hooks/useCollection'
-import { IReturnedData } from '../../ts/interfaces'
 
 import styles from './Home.module.css'
 
@@ -10,11 +9,11 @@ import TransactionList from '../../components/TransactionList'
 export default function Home() {
   const { user } = useAuthContext()
   let ID = user!.uid; // If there isn't a user, we couldn't be at the Home page.
-  const { documents, error } = useCollection('transactions')
+  const { documents, error } = useCollection('transactions', ['uid', '==', ID]); // array is for 'where' clause
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        {error && <p>{error}</p>}
+        {error && <p className={styles.error}>Problem with Request:<br /> {error}</p>}
         {documents && <TransactionList listData={documents} />}
       </div>
       <div className={styles.sidebar}>
