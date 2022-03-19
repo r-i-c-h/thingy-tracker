5570d414f496e8d787383b2c095b62569d8c04bf
r-i-c-h <rich@richwerden.com>
2022-03-19 17:04:07 -0400

    🔧🆙 useCollection to add filters to requests

--- src/hooks/useCollection.tsx
+++ src/hooks/useCollection.tsx
@@ -1,17 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { projectFirestore } from "../firebase/config";
 import { handleError } from "../ts/ErrorHandler";
 import { IReturnedData } from "../ts/interfaces";
 
+import firebase from 'firebase/app'; // <~~ Need this for the proper typing ¯\_(ツ)_/¯
 
-export const useCollection = (collection: string) => {
+type TQueryOptions = [string | firebase.firestore.FieldPath, firebase.firestore.WhereFilterOp, string]
+type TFlexibleRef = firebase.firestore.CollectionReference | firebase.firestore.Query; // if query has options it changes the type
 
+export const useCollection = (collection: string, options?: TQueryOptions) => {
   const [documents, setDocuments] = useState<null | IReturnedData[]>(null);
   const [error, setError] = useState<null | string>(null);
 
-  useEffect(() => { // useEffect means this will fire as soon as it mounts to the DOM
-    let ref = projectFirestore.collection(collection);
+  // useRef Stops infinite update 🔄 when setting options[] as a useEffect() dependency
+  const query = options ? useRef(options).current : null;
 
+
+  useEffect(() => { // useEffect will fire as soon as this mounts to the DOM
+    let ref: TFlexibleRef = projectFirestore.collection(collection); // if the 🎣 *does* have options, it changes the typing, so...
+
+    if (query) {
+      const [fieldPath, opStr, value] = [...query]
+      ref = ref.where(fieldPath, opStr, value);
+    }
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results: IReturnedData[] = [];
 
@@ -20,18 +31,17 @@ export const useCollection = (collection: string) => {
         results.push(docData)
       })
 
-      // Update State:
+      // ...Update State:
       setDocuments(results)
       setError(null);
     }, (err) => {
       setError(handleError(err));
-      console.log(err.message);
+      console.error(err.message);
     })
 
-    //  Unsub on unmoun:t
+    //  Unsub on unmount
     return () => unsubscribe()
-  }, [collection])
+  }, [collection, query])
 
   return { documents, error }
-
 }
