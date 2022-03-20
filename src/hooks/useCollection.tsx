dfa8f1a371753cb8c6996ac4fcf7d64707b80d1e
r-i-c-h <rich@richwerden.com>
2022-03-20 02:01:53 -0400

    add: 📅/⏱️-Sort to useCollection+ add to Home req

--- src/hooks/useCollection.tsx
+++ src/hooks/useCollection.tsx
@@ -5,23 +5,29 @@ import { IReturnedData } from "../ts/interfaces";
 
 import firebase from 'firebase/app'; // <~~ Need this for the proper typing ¯\_(ツ)_/¯
 
-type TQueryOptions = [string | firebase.firestore.FieldPath, firebase.firestore.WhereFilterOp, string]
+type FieldPath = firebase.firestore.FieldPath;
+type TQueryOptions = [string | FieldPath, firebase.firestore.WhereFilterOp, string]
+type TSortingOptions = [string | FieldPath, "asc" | "desc"]
 type TFlexibleRef = firebase.firestore.CollectionReference | firebase.firestore.Query; // if query has options it changes the type
 
-export const useCollection = (collection: string, options?: TQueryOptions) => {
+export const useCollection = (collection: string, options?: TQueryOptions, order?: TSortingOptions) => {
   const [documents, setDocuments] = useState<null | IReturnedData[]>(null);
   const [error, setError] = useState<null | string>(null);
 
-  // useRef Stops infinite update 🔄 when setting options[] as a useEffect() dependency
-  const query = options ? useRef(options).current : null;
+  // fuseRef Stops infinite update 🔄 when setting options[] as a useEffect() dependency
+  const queryOptions = options ? useRef(options).current : null;
+  const sortingOptions = order ? useRef(order).current : null;
 
 
   useEffect(() => { // useEffect will fire as soon as this mounts to the DOM
     let ref: TFlexibleRef = projectFirestore.collection(collection); // if the 🎣 *does* have options, it changes the typing, so...
 
-    if (query) {
-      const [fieldPath, opStr, value] = [...query]
+    if (queryOptions) {
+      const [fieldPath, opStr, value] = [...queryOptions]
       ref = ref.where(fieldPath, opStr, value);
+    }
+    if (sortingOptions) {
+      ref = ref.orderBy(...sortingOptions);
     }
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results: IReturnedData[] = [];
@@ -41,7 +47,7 @@ export const useCollection = (collection: string, options?: TQueryOptions) => {
 
     //  Unsub on unmount
     return () => unsubscribe()
-  }, [collection, query])
+  }, [collection, queryOptions, sortingOptions])
 
   return { documents, error }
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,7 +9,10 @@ import TransactionList from '../../components/TransactionList'
 export default function Home() {
   const { user } = useAuthContext()
   let ID = user!.uid; // If there isn't a user, we couldn't be at the Home page.
-  const { documents, error } = useCollection('transactions', ['uid', '==', ID]); // array is for 'where' clause
+  const { documents, error } = useCollection('transactions',
+    ['uid', '==', ID],   // array for 'where' clause
+    ['createdAt', 'desc'] // array for sorting 'orderedBy' clause
+  );
   return (
     <div className={styles.container}>
       <div className={styles.content}>
